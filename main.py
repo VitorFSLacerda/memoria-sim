bc0f269b70420e1e03f489e30c98b50479e2556e
Vitor Lacerda <floco.vitor@gmail.com>
2025-11-12 18:13:08 -0300

    Nova versão

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from mem_sim import MemorySimulator
 
-mem_simulator = MemorySimulator(page_size=4096, num_tlb_entries=16, num_frames=64, rep_policy='LRU')
+def run_simulator(arq_test):
+    mem_simulator = MemorySimulator(page_size=4096, 
+                                num_tlb_entries=16, 
+                                num_frames=64, 
+                                rep_policy='LRU')
+    for addr in arq_test:
+        mem_simulator.access_memory(int(addr))
+    mem_simulator.print_statistics()
+    print()
 
+test_files = [
+    "tests/trace.in",
+    "tests/trace_random.in",
+    "tests/trace_sequential.in"
+]
 
-arq_test = open("tests/trace.in", "r")
-
-for addr in arq_test:
-    mem_simulator.access_memory(int(addr))
-mem_simulator.print_statistics()
-
+for arq in test_files:
+   run_simulator(open(arq, "r"))
